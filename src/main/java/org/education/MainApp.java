bacefd51db976267e41edf092022452892fd2623
Maxim Isaev <iwork30rus@gmail.com>
2021-02-28 16:15:21 +0400

    1. Created a printInfromationAboutDirectory method

--- src/main/java/org/education/MainApp.java
+++ src/main/java/org/education/MainApp.java
@@ -1,17 +1,16 @@
 package org.education;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Arrays;
+import java.io.*;
+import java.util.Objects;
 
 public class MainApp {
+
     public static void writeDirectoryStructureInFile(File directory){
         {
             try(FileWriter fileWriter = new FileWriter("data/directoryInfo.txt")) {
-                for (File file : directory.listFiles()) {
+                for (File file : Objects.requireNonNull(directory.listFiles())) {
                     fileWriter.write("|-----" + file.getName() + "\n");
-                    for (File listFile : file.listFiles()) {
+                    for (File listFile : Objects.requireNonNull(file.listFiles())) {
                         fileWriter.write("|     " + listFile.getName() + "\n");
                     }
                     fileWriter.write("|\n");
@@ -21,18 +20,42 @@ public class MainApp {
             }
         }
     }
+
+    public static void printInfromationAboutDirectory(File file){
+        int countOfFiles = 0;
+        int countOfDirectory = 0;
+        int lengthName = 0;
+        try(FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+            System.out.println(bufferedReader.readLine());
+            for (File lFile : file.getParentFile().listFiles()) {
+                if (lFile.isFile() && lFile.exists()){
+                    countOfFiles++;
+                    lengthName += lFile.getName().length();
+                }
+                else if(lFile.isDirectory() && lFile.exists())
+                    countOfDirectory++;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Колиество файлов: " + countOfFiles
+                + "\nКоличество папок: " + countOfDirectory
+                + "\nСредняя длина названия файла: " + lengthName / countOfFiles);
+    }
     public static void main(String[] args) {
         String path = "C:\\\\Users\\\\iwork\\\\lesson8\\";
-        if(path != null){
-            File file = new File(path);
-            if(file.isDirectory() && file.exists()){
-                writeDirectoryStructureInFile(file);
-            }
-            else if(file.isFile() && file.exists()){
-
-            }
-            else
-                System.out.println("Файла/директории не существует");
+        path = "C:\\\\Users\\\\iwork\\\\lesson8\\firstFolder\\someDocumentsWithChars.txt";
+        File file = new File(path);
+        if(file.isDirectory() && file.exists()){
+            writeDirectoryStructureInFile(file);
         }
+        else if(file.isFile() && file.exists()){
+            printInfromationAboutDirectory(file);
+        }
+        else
+            System.out.println("Файла/директории не существует");
     }
 }
